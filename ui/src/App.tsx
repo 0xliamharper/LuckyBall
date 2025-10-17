d5cdecc88a645cc6014f2331f59a617e9e7f6742
alexmortoni <GawlikDelightful02031988xn@hotmail.com>
2025-10-17 23:31:10 +0800

    ui init

---
+++ ui/src/App.tsx
@@ -0,0 +1,23 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { config } from './config/wagmi';
+import { LuckyBallApp } from './components/LuckyBallApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale="en">
+          <LuckyBallApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
